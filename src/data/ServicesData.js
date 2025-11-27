9e8e34a7152c9db61cdea61c50a3c7fd98cab869
welson junior <eujuniorplus@gmail.com>
2025-11-27 08:35:21 -0300

    pasta onde está os serviços do musico-versão01

---
+++ src/data/ServicesData.js
@@ -0,0 +1,118 @@
+import {
+    Cake,
+    Music,
+    Heart,
+    GraduationCap,
+    PartyPopper,
+    Building2,
+    Cross,
+    Stars,
+    Mic2,
+} from "lucide-react";
+
+export const services = [
+    {
+        id: 1,
+        title: "Aniversários",
+        description:
+            "Leve emoção e alegria para sua comemoração com um repertório envolvente que transforma seu aniversário em um momento inesquecível.",
+        icon: Cake,
+        img:"../assets/img-birth.jpg"
+    },
+    {
+        id: 2,
+        title: "Aulas de Sax",
+        description:
+            "Aulas personalizadas para iniciantes e avançados, focadas em técnica, respiração, leitura musical e performance ao vivo.",
+        icon: Music,
+        img:"../assets/img-teacher.jpg"
+    },
+    {
+        id: 3,
+        title: "Casamentos",
+        description:
+            "A trilha sonora perfeita para o dia mais importante da sua vida, com músicas que emocionam e criam memórias eternas.",
+        icon: Heart,
+        img:"../assets/img-marriage.jpg"
+    },
+    {
+        id: 4,
+        title: "Confraternizações",
+        description:
+            "Transforme encontros corporativos ou familiares em experiências especiais com música suave e sofisticada ao vivo.",
+        icon: PartyPopper,
+        img:"../assets/img-events.jpg"
+    },
+    {
+        id: 5,
+        title: "Encontros de Casais",
+        description:
+            "Uma atmosfera romântica e acolhedora, ideal para jantares e momentos a dois com música instrumental ao vivo.",
+        icon: Heart,
+        img:"../assets/img-love.jpg"
+    },
+    {
+        id: 6,
+        title: "Formaturas",
+        description:
+            "Celebre uma conquista tão importante com uma apresentação marcante e cheia de emoção.",
+        icon: GraduationCap,
+        img:"../assets/img-graduation.jpg"
+    },
+    {
+        id: 7,
+        title: "Inaugurações",
+        description:
+            "Dê um toque de elegância e destaque ao seu evento com música ao vivo na abertura do seu espaço.",
+        icon: Building2,
+        img:"../assets/img-inauguration.jpg"
+    },
+    {
+        id: 8,
+        title: "Sepultamentos",
+        description:
+            "Homenagens musicais respeitosas e sensíveis, trazendo conforto, paz e emoção em momentos de despedida.",
+        icon: Cross,
+        img:"../assets/img-burial.jpg"
+    },
+    {
+        id: 9,
+        title: "Serenatas",
+        description:
+            "Surpreenda quem você ama com uma serenata inesquecível, carregada de sentimento e musicalidade.",
+        icon: Stars,
+        img:"../assets/img-serenade.jpg"
+    },
+    {
+        id: 10,
+        title: "Shows Variados",
+        description:
+            "Apresentações para bares, restaurantes, eventos e ocasiões especiais, adaptadas ao estilo do público e ambiente.",
+        icon: Mic2,
+        img:"../assets/img-shows.jpg"
+    },
+    {
+        id: 11,
+        title: "Sarau Literário",
+        description:
+            "Leve elegância e emoção para o seu Sarau Literário com uma apresentação especial de saxofone ao vivo. ",
+        icon: Mic2,
+        img:"../assets/img-sarauLiterario.jpg"
+    },
+    {
+        id: 12,
+        title: "Eventos Corporativos",
+        description:
+            "Ideal para convenções, confraternizações, inaugurações e recepções, criando um ambiente elegante e memorável para clientes, colaboradores e parceiros. ",
+        icon: Mic2,
+        img:"../assets/img-corporativo.jpg"
+    },
+    {
+        id: 13,
+        title: "Shows Intimista",
+        description:
+            "Uma apresentação exclusiva, com clima aconchegante e conexão direta com o público. Ideal para ambientes pequenos e eventos privados. ",
+        icon: Mic2,
+        img:"../assets/img-intimista.jpg"
+    },
+];
